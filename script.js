5af62284019bc0b8d9b831587bd4673ee88a7245
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-04 10:59:33 -0500

    square display

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const screenHeight = window.innerHeight;
+const screenWidth = window.innerWidth;
+const minDimension = Math.min(screenHeight, screenWidth);
+const squareSide = (minDimension - 100) / 16;
+console.log(squareSide);
 
+const grid = document.querySelector('.grid');
+
+let square = document.createElement('div');
+square.style.width = `${squareSide}px`;
+square.style.height = `${squareSide}px`;
+square.style.border = '2px solid pink';
+square.style.boxSizing = 'border-box';
+
+grid.appendChild(square);
