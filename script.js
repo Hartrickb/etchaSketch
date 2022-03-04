2ba427ccd044f10823841950f479943530353084
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-04 11:05:34 -0500

    grid of squares

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ console.log(squareSide);
 
 const grid = document.querySelector('.grid');
 
-let square = document.createElement('div');
-square.style.width = `${squareSide}px`;
-square.style.height = `${squareSide}px`;
-square.style.border = '2px solid pink';
-square.style.boxSizing = 'border-box';
-
-grid.appendChild(square);
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.style.display = 'flex';
+  grid.appendChild(row);
+  for (let j = 0; j < 16; j++) {
+    let square = document.createElement('div');
+    square.style.width = `${squareSide}px`;
+    square.style.height = `${squareSide}px`;
+    square.style.border = '2px solid pink';
+    square.style.boxSizing = 'border-box';
+    row.appendChild(square);
+  }
+}
