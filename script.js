73ad32ae8b2c07696ebac12b32b6e1e3196a0d78
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-04 13:31:52 -0500

    resize grid

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const screenHeight = window.innerHeight;
 const screenWidth = window.innerWidth;
 const minDimension = Math.min(screenHeight, screenWidth);
-const squareSide = (minDimension - 100) / 16;
-console.log(squareSide);
 
+const gridSize = document.querySelector('#size');
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  row.style.display = 'flex';
-  grid.appendChild(row);
-  for (let j = 0; j < 16; j++) {
-    let square = document.createElement('div');
-    square.style.width = `${squareSide}px`;
-    square.style.height = `${squareSide}px`;
-    square.style.border = '2px solid pink';
-    square.style.boxSizing = 'border-box';
-    square.addEventListener('mouseover', (event) => {
-      event.target.style.backgroundColor = 'blue';
-    });
-    row.appendChild(square);
+gridSize.addEventListener('mouseup', (event) => {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  createGrid(event.target.value);
+});
+
+function createGrid(size) {
+  const squareSide = (minDimension - 150) / size;
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.style.display = 'flex';
+    grid.appendChild(row);
+    for (let j = 0; j < size; j++) {
+      let square = document.createElement('div');
+      square.style.width = `${squareSide}px`;
+      square.style.height = `${squareSide}px`;
+      square.style.border = '1px solid pink';
+      square.style.boxSizing = 'border-box';
+      square.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'blue';
+      });
+      row.appendChild(square);
+    }
   }
 }
 
-function changeBackground() {}
+createGrid(32);
