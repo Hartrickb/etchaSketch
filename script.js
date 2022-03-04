09efed7d9577113df46742387443594dfe469ea3
Hartrickb <hartrickb@users.noreply.github.com>
2022-03-04 13:53:50 -0500

    clear

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ const screenHeight = window.innerHeight;
 const screenWidth = window.innerWidth;
 const minDimension = Math.min(screenHeight, screenWidth);
 
-const gridSize = document.querySelector('#size');
+const sizeSlider = document.querySelector('#sizeSlider');
 const grid = document.querySelector('.grid');
 
-gridSize.addEventListener('mouseup', (event) => {
+let gridSize = 32;
+
+// Slider value changed
+sizeSlider.addEventListener('mouseup', (event) => {
+  gridSize = event.target.value;
+  redoGrid(gridSize);
+});
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', redoGrid);
+
+// Clears the grid element and creates another
+function redoGrid() {
   while (grid.firstChild) {
     grid.removeChild(grid.firstChild);
   }
-  createGrid(event.target.value);
-});
+  createGrid(gridSize);
+}
 
+// Creates the appropriate sized grid with empty squares
 function createGrid(size) {
   const squareSide = (minDimension - 150) / size;
   for (let i = 0; i < size; i++) {
@@ -32,4 +45,5 @@ function createGrid(size) {
   }
 }
 
+// Starter grid
 createGrid(32);
